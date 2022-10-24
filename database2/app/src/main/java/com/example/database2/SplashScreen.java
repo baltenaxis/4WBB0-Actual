5445eaaa83092cf70deab254879a4093d181838d
baltenaxis <91437810+baltenaxis@users.noreply.github.com>
2022-10-24 21:06:56 +0200

    wer

--- database2/app/src/main/java/com/example/database2/SplashScreen.java
+++ database2/app/src/main/java/com/example/database2/SplashScreen.java
@@ -40,7 +40,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, InventoryHandler.class);
+                Intent intent = new Intent(SplashScreen.this, Welcome.class);
                 intent.putExtra("kur",items1);
                 startActivity(intent);
                 finish();
